901336ac4b5b574528c736157b50d634b5f1d669
vp-82 <github@pechtor.ch>
2023-06-06 16:40:06 +0200

    changed os env to string

--- ArticleService/tests/performance/test_LinkScraper_performance.py
+++ ArticleService/tests/performance/test_LinkScraper_performance.py
@@ -12,11 +12,14 @@ logger = logging.getLogger(__name__)
 
 @pytest.fixture
 def test_db():
-    if not os.getenv("CI_PIPELINE"):  # Check if the CI environment variable is set
-    # We're in the local development environment
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./tests/mtg-scraper-385015-aad053a61746.json"
-        # If we're in the CI environment, the GOOGLE_APPLICATION_CREDENTIALS
+    if os.getenv("CI") == "true":
+        # We're in the CI environment, the GOOGLE_APPLICATION_CREDENTIALS
         # environment variable is already set in the workflow file.
+        pass
+    else:
+        # We're in the local development environment
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./tests/mtg-scraper-385015-aad053a61746.json"
+
 
     db = firestore.Client()
     return db.collection('mtg_scraper_test_collection')
