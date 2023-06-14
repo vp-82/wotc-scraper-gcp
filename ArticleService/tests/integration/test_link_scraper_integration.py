e2e4f01cab0dc7d043f56df9545c1e5ecb381b0f
vp-82 <github@pechtor.ch>
2023-06-14 08:09:03 +0200

    Linted unit integration and performance tests

--- ArticleService/tests/integration/test_link_scraper_integration.py
+++ ArticleService/tests/integration/test_link_scraper_integration.py
@@ -1,6 +1,10 @@
+"""
+test_link_scraper.py: This module contains the integration tests for the Scraper class
+which is responsible for scraping article links from a webpage and storing them in a Firestore database.
+"""
+
 import logging
 import os
-from unittest.mock import MagicMock, patch
 
 import pytest
 from google.cloud import firestore
@@ -11,8 +15,18 @@ from src.link_scraper import FirestoreArticleLinkAdapter, Scraper
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def test_db():
+@pytest.fixture(name="test_db")
+def fixture_test_db():
+    """
+    Pytest fixture for setting up the Firestore database client.
+
+    Depending on the environment (CI or local), it sets the appropriate
+    Google application credentials and returns a reference to the Firestore collection.
+
+    Returns:
+        google.cloud.firestore_v1.collection.CollectionReference:
+        A reference to the Firestore collection used for testing.
+    """
     if os.getenv("CI_PIPELINE") == "true":
         # We're in the CI environment, the GOOGLE_APPLICATION_CREDENTIALS
         # environment variable is already set in the workflow file.
@@ -21,12 +35,25 @@ def test_db():
         # We're in the local development environment
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./tests/mtg-scraper-385015-aad053a61746.json"
 
-
+    # pylint: disable=invalid-name
     db = firestore.Client()
     return db.collection('mtg_scraper_test_collection')
 
+
 @pytest.mark.integration
 def test_with_real_webpage(test_db):
+    """
+    Integration test for the Scraper class with a real webpage.
+
+    This test scrapes links from a real webpage, stores them in a Firestore database,
+    and checks if the number of stored links is greater than zero. After the test,
+    it cleans up the database and checks if the number of deleted documents matches
+    with the number of stored links.
+
+    Args:
+        test_db (google.cloud.firestore_v1.collection.CollectionReference):
+        A reference to the Firestore collection used for testing.
+    """
     logger.info("Starting integration test with real webpage...")
     adapter = FirestoreArticleLinkAdapter(test_db)
     scraper = Scraper(adapter)
@@ -36,7 +63,7 @@ def test_with_real_webpage(test_db):
 
     logger.info("Storing links in the database...")
     docs = test_db.stream()
-    
+
     links = []
     for doc in docs:
         data = doc.to_dict()
@@ -59,7 +86,3 @@ def test_with_real_webpage(test_db):
     # Final test: check if the number of links matches with the number of cleaned up documents
     assert len(links) == i
     logger.info("Integration test completed successfully.")
-
-
-
-
--- ArticleService/tests/performance/test_link_scraper_performance.py
+++ ArticleService/tests/performance/test_link_scraper_performance.py
@@ -1,3 +1,9 @@
+"""
+test_link_scraper_performance.py
+
+This module contains tests for the performance of the link scraper.
+"""
+
 import logging
 import os
 import time
@@ -10,8 +16,16 @@ from src.link_scraper import FirestoreArticleLinkAdapter, Scraper
 
 logger = logging.getLogger(__name__)
 
-@pytest.fixture
-def test_db():
+
+@pytest.fixture(name="test_db")
+def fixture_test_db():
+    """
+    Pytest fixture to setup a Firestore client and return a Firestore collection.
+    The fixture checks if the environment is CI or local, and sets the credentials accordingly.
+
+    Returns:
+        Firestore collection for testing.
+    """
     if os.getenv("CI") == "true":
         # We're in the CI environment, the GOOGLE_APPLICATION_CREDENTIALS
         # environment variable is already set in the workflow file.
@@ -20,12 +34,17 @@ def test_db():
         # We're in the local development environment
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./tests/mtg-scraper-385015-aad053a61746.json"
 
-
+    # pylint: disable=invalid-name
     db = firestore.Client()
     return db.collection('mtg_scraper_test_collection')
 
+
 @pytest.mark.performance
 def test_with_real_webpage(test_db):
+    """
+    Performance test that scrapes links from a real webpage, stores the links in the Firestore database,
+    and then cleans up the database after the test.
+    """
     logger.info("Starting performance test with real webpage...")
 
     adapter = FirestoreArticleLinkAdapter(test_db)
@@ -35,14 +54,14 @@ def test_with_real_webpage(test_db):
     start_time = time.time()  # Save the current time
     scraper.scrape_links(1, 10)
     elapsed_time = time.time() - start_time  # Calculate the elapsed time
-    logger.info(f"Finished scraping links, it took {elapsed_time} seconds.")
+    logger.info("Finished scraping links, it took %s seconds.", elapsed_time)
 
     logger.info("Storing links in the database...")
     start_time = time.time()
     docs = test_db.stream()
     elapsed_time = time.time() - start_time
-    logger.info(f"Stored links in the database, it took {elapsed_time} seconds.")
-    
+    logger.info("Stored links in the database, it took %s seconds.", elapsed_time)
+
     links = []
     for doc in docs:
         data = doc.to_dict()
@@ -63,7 +82,7 @@ def test_with_real_webpage(test_db):
         doc.reference.delete()
         i += 1
     elapsed_time = time.time() - start_time
-    logger.info(f"Cleaned up the database after test, it took {elapsed_time} seconds.")
+    logger.info("Cleaned up the database after test, it took %s seconds.", elapsed_time)
 
     # Final test: check if the number of links matches with the number of cleaned up documents
     assert len(links) == i
